adc5feead21cfe746329dada98138a258847cbce
Emmy <hmemmy209@gmail.com>
2025-01-20 21:30:09 +0100

    Initial commit

---
+++ src/main/java/me/emmy/project/PasswordGenerator.java
@@ -0,0 +1,12 @@
+package me.emmy.project;
+
+/**
+ * @author Emmy
+ * @project Default (Template) Project
+ * @date 20/01/2025 - 21:17
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
